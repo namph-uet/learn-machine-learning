7a566ba356f97a8f8a69d68fe1d1645569173569
namph-uet <hoangnamuet.vnu@gmail.com>
2021-03-10 21:17:44 +0700

    done numpy baic

--- learn-numpy.py
+++ learn-numpy.py
@@ -36,3 +36,32 @@ print(awesome[:, 1])
 #Access the array in the matrix
 print(awesome[1:3, 2:4])
 print(awesome[0,1])
+
+#Boardcasting
+
+numbers_1D = np.array([1,2,3])
+print('------------------------------------')
+print(numbers_1D, ' + 3 = ', numbers_1D + 3)
+print(numbers_1D, ' -3 3 = ', numbers_1D - 3)
+numbers_1D_range = np.arange(10)
+print(numbers_1D_range, ' * 10 =', numbers_1D_range * 10)
+
+print('boardcasting with 2D matrix')
+numbers_2D_ones = np.ones((10,10))
+print(numbers_2D_ones)
+print(numbers_2D_ones, ' * 10 = ', numbers_2D_ones * 10)
+
+#Addition 2 arrays of different dimensions
+print('Addition 2 arrays of different dimensions')
+x = np.array([
+    [6,9,10],
+    [15,18,20],
+])
+y = np.array([
+    [2],
+    [4]
+])
+
+#could not be broadcast together with shapes (2,3) (2,)
+z = np.array([1,2])
+#print(x + z)
